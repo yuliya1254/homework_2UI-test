d2475153db5e6fed72d142cfa8c93fde0557a6c8
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-10-06 00:16:05 +0300

    add Page Object for Automation_practice_form

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,14 +1,13 @@
 package pages;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import pages.components.BeforeAllComponent;
 import pages.components.CalendarComponent;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationPage {
 
@@ -19,64 +18,125 @@ public class RegistrationPage {
     SelenideElement
 
             firstNameInput = $("#firstName"),
-            lastNameInput =  $("#lastName"),
+            lastNameInput = $("#lastName"),
             userEmailInput = $("#userEmail"),
             genderWrapperInput = $("#genterWrapper"),
-            userNumberInput = $("#userNumber");
+            userNumberInput = $("#userNumber"),
+            subjectsInput = $("#subjectsInput"),
+            hobbies = $("#hobbiesWrapper"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddress = $("#currentAddress"),
+            state = $("#state"),
+            stateCity = $("#stateCity-wrapper"),
+            city = $("#city"),
+            submit = $("#submit"),
+            formResult = $(".table-responsive"),
+            modalForm = $(".modal-dialog"),
+            modalFormTitle = $("#example-modal-sizes-title-lg");
 
 
     //Actions
 
 
-    public RegistrationPage beforeAllCase (){
+    public RegistrationPage beforeAllCase() {
         beforeAll.beforeAllCase();
 
         return this;
     }
-    public RegistrationPage openPage (){
+
+    public RegistrationPage openPage() {
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
         return this;
     }
-    public RegistrationPage setFirstNameInput(String value){
+
+    public RegistrationPage setFirstNameInput(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
 
-    public RegistrationPage setLastNameInput (String value){
+    public RegistrationPage setLastNameInput(String value) {
         lastNameInput.setValue(value);
         return this;
 
     }
 
-    public RegistrationPage setUserEmailInput(String value){
+    public RegistrationPage setUserEmailInput(String value) {
         userEmailInput.setValue(value);
         return this;
 
     }
 
-    public RegistrationPage setGenderWrapperInput(String value){
+    public RegistrationPage setGenderWrapperInput(String value) {
         genderWrapperInput.$(byText(value)).click();
         return this;
 
     }
 
-    public RegistrationPage setUserNumberInput(String value){
+    public RegistrationPage setUserNumberInput(String value) {
         userNumberInput.setValue(value);
         return this;
 
     }
 
-    public RegistrationPage setDateOfBirt(String day,String month, String year){
+    public RegistrationPage setDateOfBirt(String day, String month, String year) {
         $("#dateOfBirthInput").click();
-        calendar.setDate("19","June", "1994" );
+        calendar.setDate("19", "June", "1994");
         return this;
 
     }
 
+    public RegistrationPage setSubjectsInput(String value) {
+        subjectsInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value) {
+        hobbies.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setUploadPicture(String value) {
+        uploadPicture.uploadFromClasspath("test.jpg");
+        return this;
+    }
+
+    public RegistrationPage setCurrentAddress(String value) {
+        currentAddress.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setState(String value) {
+        state.click();
+        stateCity.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setCity(String value) {
+        city.click();
+        stateCity.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage clickSubmit() {
+        submit.click();
+        return this;
+    }
+
+    public RegistrationPage checkModalForm(String value) {
+        modalForm.should(appear);
+        modalFormTitle.shouldHave(text(value));
+        return this;
+    }
+
+
+    public RegistrationPage checkResult(String key, String value) {
+        formResult.$(byText(key)).parent().shouldHave(text(value));
+        return this;
+    }
 
 
 }
--- src/test/java/tests/Automation_practice_form.java
+++ src/test/java/tests/Automation_practice_form.java
@@ -1,63 +1,92 @@
 package tests;
 
-import com.codeborne.selenide.selector.ByText;
-import org.junit.jupiter.api.BeforeAll;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 
 
 public class Automation_practice_form {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
-        @Test
-        void fillFormTest() {
+    @Test
+    void fillFormTest() {
 
-           registrationPage.beforeAllCase ()
-                            .openPage()
-                            .setFirstNameInput("Ivan")
-                            .setLastNameInput("Ivanov")
-                            .setUserEmailInput("Ivanov@gmail.ru")
-                            .setGenderWrapperInput("Male")
-                            .setUserNumberInput("8988888888")
-                            .setDateOfBirt("19","June", "1994" );
-
-
-            $("#dateOfBirthInput").click();
-            $(".react-datepicker__month-dropdown-container").$(byText("June")).click();
-            $(".react-datepicker__year-select").selectOption("1994");
-            $(".react-datepicker__day--019").click();
-            $("#subjectsInput").setValue("maths").pressEnter();
-            $("#hobbiesWrapper").$(byText("Sports")).click();
-            $("#uploadPicture").uploadFromClasspath("test.jpg");
-            $("#currentAddress").setValue("my Adress");
-            $("#state").click();
-            $("#stateCity-wrapper").$(byText("NCR")).click();
-            $("#city").click();
-            $("#stateCity-wrapper").$(byText("Delhi")).click();
-            $("#submit").click();
-
-            $(".table-responsive").shouldHave(
-                    text("Ivan Ivanov"),
-                    text("Ivanov@gmail.ru"),
-                    text("Male"),
-                    text("8988888888"),
-                    text("19 June,1994"),
-                    text("maths"),
-                    text("Sports"),
-                    text("test.jpg"),
-                    text("my Adress"),
-                    text("NCR Delhi"));
+        registrationPage.beforeAllCase()
+                .openPage()
+                .setFirstNameInput("Ivan")
+                .setLastNameInput("Ivanov")
+                .setUserEmailInput("Ivanov@gmail.ru")
+                .setGenderWrapperInput("Male")
+                .setUserNumberInput("8988888888")
+                .setDateOfBirt("19", "June", "1994")
+                .setSubjectsInput("maths")
+                .setHobbies("Sports")
+                .setUploadPicture("test.jpg")
+                .setCurrentAddress("my Adress")
+                .setState("NCR")
+                .setCity("Delhi")
+                .clickSubmit()
+                .checkModalForm("Thanks for submitting the form")
 
 
-        }
+                .checkResult("Student Name", "Ivan Ivanov")
+                .checkResult("Student Email", "Ivanov@gmail.ru")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8988888888")
+                .checkResult("Date of Birth", "19 June,1994")
+                .checkResult("Subjects", "Maths")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "test.jpg")
+                .checkResult("Address", "my Adress")
+                .checkResult("State and City", "NCR Delhi");
+
+
     }
 
+
+@Test
+    void fillFormTestMondatory() {
+
+        registrationPage.beforeAllCase()
+                .openPage()
+                .setFirstNameInput("Ivan")
+                .setLastNameInput("Ivanov")
+                .setGenderWrapperInput("Male")
+                .setUserNumberInput("8988888888")
+                .setDateOfBirt("19", "June", "1994")
+                .clickSubmit()
+                .checkModalForm("Thanks for submitting the form")
+
+
+                .checkResult("Student Name", "Ivan Ivanov")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8988888888")
+                .checkResult("Date of Birth", "19 June,1994");
+
+
+    }
+
+    @Test
+    void fillFormTestFaled() {
+
+        registrationPage.beforeAllCase()
+                .openPage()
+                .setLastNameInput("Ivanov")
+                .setGenderWrapperInput("Male")
+                .setUserNumberInput("8988888888")
+                .setDateOfBirt("19", "June", "1994")
+                .clickSubmit()
+                .checkModalForm("Thanks for submitting the form")
+
+
+                .checkResult("Student Name", "Ivan Ivanov")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8988888888")
+                .checkResult("Date of Birth", "19 June,1994");
+
+
+    }
+}
 
 
 
