66bd38478f4b7dbae1e533b959530b0f37af9f9a
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-10-22 19:44:25 +0300

    add Attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,12 +3,18 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+
 public class TestBase {
+
     @BeforeEach
     public void beforeAllCase() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -24,9 +30,18 @@ public class TestBase {
     }
 
     @AfterEach
-    void afterEach () {
-            Selenide.clearBrowserCookies();
-            Selenide.clearBrowserLocalStorage();
-
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
-}
+    @AfterEach
+    void afterEach() {
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+
+        }
+    }
+
