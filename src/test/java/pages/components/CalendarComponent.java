45e9e43c46414c48da63d6d310293a90f5199e3a
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-10-29 12:27:28 +0300

    поправила календарь

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,6 +7,6 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-dropdown-container").$(byText(month)).click();
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--0" + day).click();
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
     }
 }
